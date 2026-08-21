2ac18077929bc60f73f63c281ae223c99df3f430
abi1227-ma <abikuti742@gmail.com>
2026-08-21 10:14:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+// =========================
+// PORTFOLIO JAVASCRIPT
+// =========================
+
+
+// Check that JavaScript is connected
+console.log("Abilaksha's Portfolio Loaded Successfully!");
+
+
+// =========================
+// CURRENT YEAR
+// =========================
+
+const currentYear = new Date().getFullYear();
+
+document.querySelector("footer p").textContent =
+    `© ${currentYear} Abilaksha Thirugnanasambandam`;
